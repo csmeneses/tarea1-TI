a0d55a47c9a73c651e60f546d70b09f59484b2e3
Carlos Meneses <csmeneses@uc.cl>
2021-04-06 01:08:16 -0400

    vamos que se puede

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
-    def home
-    end
+  def home
+    base_url = 'https://tarea-1-breaking-bad.herokuapp.com/api/'
+    response = RestClient.get(base_url + 'characters/1')
+    @results = JSON.parse(response.to_str)
+    # name = results['forms'][0]['name']
+  end
 end

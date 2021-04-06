b31e71ed2863972d64a4911412619d65bfbc9e3d
Carlos Meneses <csmeneses@uc.cl>
2021-04-06 12:32:22 -0400

    2 de 5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,40 @@
 class StaticPagesController < ApplicationController
+  @@base_url = 'https://tarea-1-breaking-bad.herokuapp.com/api/'
   def home
-    base_url = 'https://tarea-1-breaking-bad.herokuapp.com/api/'
-    response = RestClient.get(base_url + 'characters/1')
-    @results = JSON.parse(response.to_str)
-    # name = results['forms'][0]['name']
+    response = RestClient.get(@@base_url + 'episodes?series=Breaking+Bad')
+    bb_episodes = JSON.parse(response.to_str)
+    response = RestClient.get(@@base_url + 'episodes?series=Better+Call+Saul')
+    bcs_episodes = JSON.parse(response.to_str)
+    seasons = {bb: [], bcs: []}
+    bb_episodes.each do |epi|
+      if !seasons[:bb].include? epi["season"].to_i
+        seasons[:bb] << epi["season"].to_i
+      end
+    end
+    bcs_episodes.each do |epi|
+      if !seasons[:bcs].include? epi["season"].to_i
+        seasons[:bcs] << epi["season"].to_i
+      end
+    end
+    @results = seasons
+  end
+
+  def season_show
+    season = params[:season]
+    serie = params[:serie]
+    if serie == "bb"
+      serie_name = "Breaking+Bad"
+    elsif serie == "bcs"
+      serie_name = "Better+Call+Saul"
+    end
+    response = RestClient.get(@@base_url + "episodes?series=#{serie_name}")
+    episodes = JSON.parse(response.to_str)
+    season_episodes = []
+    episodes.each do |epi|
+      if epi["season"] == season
+        season_episodes << epi
+      end
+    end
+    @results = season_episodes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "static_pages/home", to: "static_pages#home", as: "home"
+  get "static_pages/season_show/:season/:serie", to: "static_pages#season_show", as: "season_show" 
 
-  root :to => "static_pages#home"
+  root to: "static_pages#home"
 end
